6d6f95a951e2fd23f00eb1f0e943f70a07029e49
Soniii-05 <dicesaresonia2005@gmail.com>
2026-01-09 11:03:51 +0100

    add menu and game instance

---
+++ Source/TheGautlet2/MyGameInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "MyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGAUTLET2_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public: 
+	UFUNCTION(BlueprintCallable)
+	void StartGame();
+
+	UFUNCTION(BlueprintCallable)
+	void MainMenu();
+};
+	void UMyGameInstance::StartGame()
+	{
+		UGameplayStatics::OpenLevel(this, FName("Lvl_ThirdPerson"));
+	}
+
+	void UMyGameInstance::MainMenu()
+	{
+		UGameplayStatics::OpenLevel(this, FName("Lvl_MainMenu"));
+	}
